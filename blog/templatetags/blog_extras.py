149e89119d98047e174548e8868c808e5f81ee04
ivan Dubograi <ivand1988@outlook.com>
2021-11-23 09:43:12 +0000

    Finish Introduction and Django Admin

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,8 +4,9 @@ from django import template
 from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
 from blog.models import Post
+import logging 
 
-
+logger = logging.getLogger(__name__)
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
@@ -53,4 +54,5 @@ def endcol():
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
