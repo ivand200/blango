c3e83433a801c9171a721a347ead250debf8f81b
ivan Dubograi <ivand1988@outlook.com>
2021-11-28 12:10:30 +0000

    Finish Introduction and Django Admin

--- blango/settings.py
+++ blango/settings.py
@@ -51,13 +51,16 @@ class  Dev(Configuration):
       'django.contrib.sessions',
       'django.contrib.messages',
       'django.contrib.staticfiles',
+    
       'blog',
 
       'crispy_forms',
       'crispy_bootstrap5',
+      'debug_toolbar',
   ]
 
   MIDDLEWARE = [
+      'debug_toolbar.middleware.DebugToolbarMiddleware',
       'django.middleware.security.SecurityMiddleware',
       'django.contrib.sessions.middleware.SessionMiddleware',
       'django.middleware.common.CommonMiddleware',
@@ -66,6 +69,8 @@ class  Dev(Configuration):
       'django.contrib.messages.middleware.MessageMiddleware',
       #'django.middleware.clickjacking.XFrameOptionsMiddleware',
   ]
+  
+  INTERNAL_IPS = ["192.168.11.179"]
 
   ROOT_URLCONF = 'blango.urls'
 
@@ -191,7 +196,6 @@ class  Dev(Configuration):
         "level": "DEBUG",
     },
     }
-
 
   
 class Prod(Dev):
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+=-from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone 
 from blog.models import Post
 from blog.forms import CommentForm 
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 
 def index(request):
-  posts = Post.objects.filter(published_at__lte=timezone.now())
+  posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('-published_at').select_related("author")
   logger.debug("Got %d posts", len(posts))
   return render(request, "blog/index.html", {"posts": posts})
 
@@ -35,3 +35,8 @@ def post_detail(request, slug):
   return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
+
+
+def get_ip(request):
+  from django.http import HttpResponse 
+  return HttpResponse(request.META['REMOTE_ADDR'])
